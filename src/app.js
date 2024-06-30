1483766be73ba0085d68ad39d905fc7b76e7dbf2
B2005848 <namltgt2002@gmail.com>
2024-06-30 19:55:42 +0700

    update 30/03/2024

--- src/app.js
+++ src/app.js
@@ -1,6 +1,13 @@
 const express = require("express");
 const cors = require("cors");
+const userRouter = require("./Routers/user.route");
 
+const {
+  resourceNotFound,
+  methodNotAllowed,
+  handleError,
+} = require("./Controllers/errors.controller");
+// create app use express
 const app = express();
 
 app.use(cors());
@@ -12,5 +19,11 @@ app.get("/", (req, res) => {
     message: "Welcome to my project ",
   });
 });
+
+app.use("/api/users", userRouter);
+
+app.use(resourceNotFound);
+app.use(methodNotAllowed);
+app.use(handleError);
 
 module.exports = app;
