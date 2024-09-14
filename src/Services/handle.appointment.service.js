2595e8c40c4d7a4b7d617686b1022b116e040e00
B2005848 <namltgt2002@gmail.com>
2024-09-14 19:46:41 +0700

    update

--- src/Services/handle.appointment.service.js
+++ src/Services/handle.appointment.service.js
@@ -1,6 +1,25 @@
 const { knex } = require("../../db.config");
 require("dotenv").config();
 
-const handleBookingService = {};
+const handleBookingService = {
+  async AppointmentBooking(bookingData) {
+    try {
+      const resultBooking = await knex("APPOINTMENTS").insert(bookingData);
+      if (resultBooking) {
+        console.log(
+          `Booking ID: ${resultBooking[0]} has been successfully booked`,
+          [resultBooking]
+        );
+        return {
+          status: true,
+          message: "Booking Successfull",
+          data: [resultBooking],
+        };
+      }
+    } catch (error) {
+      console.error("Error occured booking", error);
+    }
+  },
+};
 
 module.exports = handleBookingService;
