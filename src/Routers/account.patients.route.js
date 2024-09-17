36f2f708d9529455c1424b12d4c49c0f6030b357
B2005848 <namltgt2002@gmail.com>
2024-09-17 20:43:48 +0700

    TESTTTTTTTTTTTTTTT

--- src/Routers/account.patients.route.js
+++ src/Routers/account.patients.route.js
@@ -2,6 +2,7 @@ const express = require("express");
 const accountPatientControllers = require("../Controllers/account.patients.controller");
 const router = express.Router();
 
+// =========CREATE ACCOUNT PATIENT============
 router.post("/create", accountPatientControllers.createAccount);
 router.post("/login", accountPatientControllers.checkLogin);
 router.put("/status/update/:id", accountPatientControllers.updateStatusAccount);
